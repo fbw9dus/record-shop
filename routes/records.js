e700f4b7e8b65e85f349498ffef14ae25aed5b25
Vasileios Psychas <vaspsych@gmail.com>
2019-09-06 17:25:50 +0200

    GET all records

--- routes/records.js
+++ routes/records.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('data/db.json');
+const db = low(adapter);
 
 /**
  * GET all records
  */
 router.get('/', function(req, res, next) {
-    res.send('Here we shall return the records');
+    const records = db.get('records')
+        .value()
+
+    res.status(200).send(records)
 });
 
 
