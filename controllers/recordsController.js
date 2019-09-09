d1888718329fc0144c05ef45737d60675f8f970a
Vasileios Psychas <vaspsych@gmail.com>
2019-09-09 11:36:50 +0200

    Added controllers

---
+++ controllers/recordsController.js
@@ -0,0 +1,21 @@
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('data/db.json');
+const db = low(adapter);
+
+
+exports.getRecords = async (req, res, next) => {
+    const records = db.get('records').value()
+    res.status(200).send(records);
+}
+
+
+exports.addRecord = async (req, res, next) => {
+    const record = req.body;
+    db.get('records').push(record)
+        .last()
+        .assign({ id: Date.now().toString() })
+        .write()
+
+    res.status(200).send(record);
+}
--- routes/records.js
+++ routes/records.js
@@ -4,28 +4,17 @@ const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const adapter = new FileSync('data/db.json');
 const db = low(adapter);
+const { getRecords, addRecord } = require('../controllers/recordsController');
+
 
 /**
  * GET all records
  */
-router.get('/', function(req, res, next) {
-    const records = db.get('records')
-        .value()
-    res.status(200).send(records)
-});
-
+router.get('/', getRecords);
 
 /**
- * POST a record
+* POST a record
  */
-router.post('/', function(req, res, next) {
-    let record = req.body;
-    db.get('records').push(record)
-        .last()
-        .assign({ id: Date.now().toString() })
-        .write()
-        
-    res.status(200).send(record);
-});
+router.post('/', addRecord);
 
 module.exports = router;
