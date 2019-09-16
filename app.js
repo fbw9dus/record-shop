d9b0676d3ea1637236eb566f0078a562c4107164
Vasileios Psychas <vaspsych@gmail.com>
2019-09-16 16:52:09 +0200

    Error handling

--- app.js
+++ app.js
@@ -23,10 +23,10 @@ app.use(logger('dev'));
 /** SETTING UP LOWDB */
 const adapter = new FileSync('data/db.json');
 const db = low(adapter);
-db.defaults({ 
-    records:[], 
-    users:[], 
-    orders:[]
+db.defaults({
+    records: [],
+    users: [],
+    orders: []
 }).write();
 
 
@@ -45,6 +45,22 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/records', recordsRouter);
 app.use('/orders', ordersRouter);
+
+/** ERROR HANDLING */
+app.use(function (req, res, next) {
+    const error = new Error('Looks like something broke...');
+    error.status = 400;
+    next(error);
+});
+
+app.use(function (err, req, res, next) {
+    res.send({
+        error: {
+            message: err.message
+        }
+    });
+});
+
 
 /** EXPORT PATH */
 module.exports = app;
