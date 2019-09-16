d7a14e5e9930185cf0fabde99424d72e82cf7fe7
Vasileios Psychas <vaspsych@gmail.com>
2019-09-16 17:14:45 +0200

    Removed async

--- controllers/recordsController.js
+++ controllers/recordsController.js
@@ -4,13 +4,13 @@ const adapter = new FileSync('data/db.json');
 const db = low(adapter);
 
 
-exports.getRecords = async (req, res, next) => {
+exports.getRecords = (req, res, next) => {
     const records = db.get('records').value()
     res.status(200).send(records);
 }
 
 
-exports.addRecord = async (req, res, next) => {
+exports.addRecord = (req, res, next) => {
     const record = req.body;
     db.get('records').push(record)
         .last()
