586d14d08a2a9f0a6071f53f8d589cca8385c8a5
Vasileios Psychas <vaspsych@gmail.com>
2019-09-23 15:37:53 +0200

    Added records model

--- controllers/recordsController.js
+++ controllers/recordsController.js
@@ -1,46 +1,52 @@
-const low = require("lowdb");
-const FileSync = require("lowdb/adapters/FileSync");
-const adapter = new FileSync("data/db.json");
-const db = low(adapter);
+const Record = require("../models/Record");
+const createError = require("http-errors");
 
-exports.getRecords = (req, res, next) => {
-  const records = db.get("records").value();
-  res.status(200).send(records);
+exports.getRecords = async (req, res, next) => {
+  try {
+    const records = await Record.find();
+    res.status(200).send(records);
+  } catch (e) {
+    next(e);
+  }
 };
 
-exports.getRecord = (req, res, next) => {
-  const { id } = req.params;
-  const record = db.get("records").find({ id });
-  res.status(200).send(record);
+exports.getRecord = async (req, res, next) => {
+  try {
+    const record = await Record.findById(req.params.id);
+    if (!record) throw new createError.NotFound();
+    res.status(200).send(record);
+  } catch (e) {
+    next(e);
+  }
 };
 
-exports.deleteRecord = (req, res, next) => {
-  const { id } = req.params;
-  const record = db
-    .get("records")
-    .remove({ id })
-    .write();
-  res.status(200).send(record);
+exports.deleteRecord = async (req, res, next) => {
+  try {
+    const record = await Record.findByIdAndDelete(req.params.id);
+    if (!record) throw new createError.NotFound();
+    res.status(200).send(record);
+  } catch (e) {
+    next(e);
+  }
 };
 
-exports.updateRecord = (req, res, next) => {
-  const { id } = req.params;
-  const dt = req.body;
-  const record = db
-    .get("records")
-    .find({ id })
-    .assign(dt)
-    .write();
-  res.status(200).send(record);
+exports.updateRecord = async (req, res, next) => {
+  try {
+    const record = await Record.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    });
+    res.status(200).send(record);
+  } catch (e) {
+    next(e);
+  }
 };
 
-exports.addRecord = (req, res, next) => {
-  const record = req.body;
-  db.get("records")
-    .push(record)
-    .last()
-    .assign({ id: Date.now().toString() })
-    .write();
-
-  res.status(200).send(record);
+exports.addRecord = async (req, res, next) => {
+  try {
+    const record = new Record(req.body);
+    await record.save();
+    res.status(200).send(record);
+  } catch (e) {
+    next(e);
+  }
 };
